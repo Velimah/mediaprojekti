830bb62b22689265833a0a0cb27f5e7cb73b0a12
Veli-Matti Heino <vekkis@gmail.com>
2023-11-01 12:16:08 +0200

    Add vitest

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Home from './views/Home';
 import Layout from './views/Layout';
 import ScrollToTop from './hooks/ScrollHook';
 
+export function sum(a: number, b: number) {
+  return a + b
+}
+
 const App = () => {
   return (
     <HashRouter>
