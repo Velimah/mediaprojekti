90797c43a3f5827788df709e7698e3df131e47fc
Veli-Matti Heino <vekkis@gmail.com>
2023-11-15 17:22:40 +0200

    Change edit inputs to text area and styling

--- src/components/EditForms.tsx
+++ src/components/EditForms.tsx
@@ -62,54 +62,55 @@ const EditForms: React.FC<EditFormsProps> = ({ originalFormValues }) => {
 	};
 
 	return (
-		<div className="py-4 space-x-2 flex flex-wrap justify-center">
+	<div className="py-4 space-x-2 flex flex-col flex-wrap justify-center items-center">
 		<button
 			onClick={editHead}
-			className="bg-black text-white py-2 px-4 rounded m-1  hover:bg-green-500"
+			className="bg-black text-white p-2 mb-4 rounded w-64  hover:bg-green-500"
 		>
 			Redo Head Tag Information
 		</button>
-		<button onClick={() => toggleSection('navigation')} className={`py-2 px-4 rounded m-1 hover:bg-green-500 ${activeSection === 'navigation' ? 'bg-green-500 text-white' : 'bg-black text-white'}`}>
-				{activeSection === 'navigation' ? 'Close Form' : 'Edit Navigation'}
-			</button>
-			
-			<button onClick={() => toggleSection('welcome')} className={`py-2 px-4 rounded m-1 hover:bg-green-500 ${activeSection === 'welcome' ? 'bg-green-500 text-white' : 'bg-black text-white'}`}>
-				{activeSection === 'welcome' ? 'Close Form' : 'Edit Welcome'}
-			</button>
-				
-			<button onClick={() => toggleSection('main')} className={`py-2 px-4 rounded m-1 hover:bg-green-500 ${activeSection === 'main' ? 'bg-green-500 text-white' : 'bg-black text-white'}`}>
-				{activeSection === 'main' ? 'Close Form' : 'Edit Main'}
-			</button>
-								
-			<button onClick={() => toggleSection('table')} className={`py-2 px-4 rounded m-1 hover:bg-green-500 ${activeSection === 'table' ? 'bg-green-500 text-white' : 'bg-black text-white'}`}>
-				{activeSection === 'table' ? 'Close Form' : 'Edit Table'}
-			</button>
-							
-			<button onClick={() => toggleSection('map')} className={`py-2 px-4 rounded m-1 hover:bg-green-500 ${activeSection === 'map' ? 'bg-green-500 text-white' : 'bg-black text-white'}`}>
-				{activeSection === 'map' ? 'Close Form' : 'Edit Map'}
-			</button>
+    <div className="flex justify-center flex-wrap">
+      <button onClick={() => toggleSection('navigation')} className={`py-2 px-4 rounded m-1 hover:bg-green-500 ${activeSection === 'navigation' ? 'bg-green-500 text-white' : 'bg-black text-white'}`}>
+          {activeSection === 'navigation' ? 'Close Form' : 'Edit Navigation'}
+        </button>
+        
+        <button onClick={() => toggleSection('welcome')} className={`py-2 px-4 rounded m-1 hover:bg-green-500 ${activeSection === 'welcome' ? 'bg-green-500 text-white' : 'bg-black text-white'}`}>
+          {activeSection === 'welcome' ? 'Close Form' : 'Edit Welcome'}
+        </button>
+          
+        <button onClick={() => toggleSection('main')} className={`py-2 px-4 rounded m-1 hover:bg-green-500 ${activeSection === 'main' ? 'bg-green-500 text-white' : 'bg-black text-white'}`}>
+          {activeSection === 'main' ? 'Close Form' : 'Edit Main'}
+        </button>
+                  
+        <button onClick={() => toggleSection('table')} className={`py-2 px-4 rounded m-1 hover:bg-green-500 ${activeSection === 'table' ? 'bg-green-500 text-white' : 'bg-black text-white'}`}>
+          {activeSection === 'table' ? 'Close Form' : 'Edit Table'}
+        </button>
+                
+        <button onClick={() => toggleSection('map')} className={`py-2 px-4 rounded m-1 hover:bg-green-500 ${activeSection === 'map' ? 'bg-green-500 text-white' : 'bg-black text-white'}`}>
+          {activeSection === 'map' ? 'Close Form' : 'Edit Map'}
+        </button>
 
-			<button onClick={() => toggleSection('footer')} className={`py-2 px-4 rounded m-1 hover:bg-green-500 ${activeSection === 'footer' ? 'bg-green-500 text-white' : 'bg-black text-white'}`}>
-				{activeSection === 'footer' ? 'Close Form' : 'Edit Footer'}
-			</button>
+        <button onClick={() => toggleSection('footer')} className={`py-2 px-4 rounded m-1 hover:bg-green-500 ${activeSection === 'footer' ? 'bg-green-500 text-white' : 'bg-black text-white'}`}>
+          {activeSection === 'footer' ? 'Close Form' : 'Edit Footer'}
+        </button>
+      </div>
 
 			{activeSection === 'navigation' && (
 			<form
-			className="bg-gray-200 p-4 rounded-b-md"
+			className="bg-gray-200 m-2 p-4 rounded-b-md flex flex-col"
 			onSubmit={(event) => handleEditForm('createNavigation', event)}
 			>
-				<label className="relative"> Navigation
-				<input
-					type="text"
+								<textarea
+        rows={3}
+        cols={80}
 					placeholder="Give additional information"
 					value={formStateValues.additionalInfo}
 					onChange={(event) => setFormStateValues({ ...formStateValues, additionalInfo: event.target.value })}
-					className="rounded-md border-black py-3 pl-12 pr-3 placeholder-grey-400 placeholder:italic placeholder:truncate focus:outline-none focus:border-black focus:ring-black focus:ring-1 w-full"
+					className="rounded-md border-black p-3 placeholder-grey-400 placeholder:italic placeholder:truncate focus:outline-none focus:border-black focus:ring-black focus:ring-1 w-full"
 				/>
-				</label>
 				<button
 					type="submit"
-					className="bg-black text-white py-2 px-4 rounded m-1 hover:bg-green-500"
+					className="bg-black text-white py-2 rounded mt-2 hover:bg-green-500"
 				>
 				Redo Navigation
 				</button>
@@ -118,21 +119,20 @@ const EditForms: React.FC<EditFormsProps> = ({ originalFormValues }) => {
 
 			{activeSection === 'welcome' && (
 			<form
-			className="bg-gray-200 p-4 rounded-b-md"
+			className="bg-gray-200 p-4 m-2 rounded-md flex flex-col"
 			onSubmit={(event) => handleEditForm('createWelcomeSection', event)}
 			>
-				<label className="relative"> Welcome
-				<input
-					type="text"
+				<textarea
+        rows={3}
+        cols={80}
 					placeholder="Give additional information"
 					value={formStateValues.additionalInfo}
 					onChange={(event) => setFormStateValues({ ...formStateValues, additionalInfo: event.target.value })}
-					className="rounded-md border-black py-3 pl-12 pr-3 placeholder-grey-400 placeholder:italic placeholder:truncate focus:outline-none focus:border-black focus:ring-black focus:ring-1 w-full"
+					className="rounded-md border-black p-3 placeholder-grey-400 placeholder:italic placeholder:truncate focus:outline-none focus:border-black focus:ring-black focus:ring-1 w-full"
 				/>
-				</label>
 				<button
 					type="submit"
-					className="bg-black text-white py-2 px-4 rounded m-1 hover:bg-green-500"
+					className="bg-black text-white py-2 rounded mt-2 hover:bg-green-500"
 				>
 				Redo Welcome
 				</button>
@@ -141,21 +141,20 @@ const EditForms: React.FC<EditFormsProps> = ({ originalFormValues }) => {
 
 			{activeSection === 'main' && (
 			<form
-			className="bg-gray-200 p-4 rounded-b-md"
+			className="bg-gray-200 p-4 m-2 rounded-md flex flex-col"
 			onSubmit={(event) => handleEditForm('createMainSection', event)}
 			>
-				<label className="relative"> Main
-				<input
-					type="text"
+				<textarea
+        rows={3}
+        cols={80}
 					placeholder="Give additional information"
 					value={formStateValues.additionalInfo}
 					onChange={(event) => setFormStateValues({ ...formStateValues, additionalInfo: event.target.value })}
-					className="rounded-md border-black py-3 pl-12 pr-3 placeholder-grey-400 placeholder:italic placeholder:truncate focus:outline-none focus:border-black focus:ring-black focus:ring-1 w-full"
+					className="rounded-md border-black p-3 placeholder-grey-400 placeholder:italic placeholder:truncate focus:outline-none focus:border-black focus:ring-black focus:ring-1 w-full"
 				/>
-				</label>
 				<button
 					type="submit"
-					className="bg-black text-white py-2 px-4 rounded m-1 hover:bg-green-500"
+					className="bg-black text-white py-2 rounded mt-2 hover:bg-green-500"
 				>
 				Redo Main
 				</button>
@@ -164,21 +163,20 @@ const EditForms: React.FC<EditFormsProps> = ({ originalFormValues }) => {
 
 			{activeSection === 'map' && (
 			<form
-			className="bg-gray-200 p-4 rounded-b-md"
+			className="bg-gray-200 p-4 m-2 rounded-md flex flex-col"
 			onSubmit={(event) => handleEditForm('createMap', event)}
 			>
-				<label className="relative"> Map
-				<input
-					type="text"
+				<textarea
+        rows={3}
+        cols={80}
 					placeholder="Give additional information"
 					value={formStateValues.additionalInfo}
 					onChange={(event) => setFormStateValues({ ...formStateValues, additionalInfo: event.target.value })}
-					className="rounded-md border-black py-3 pl-12 pr-3 placeholder-grey-400 placeholder:italic placeholder:truncate focus:outline-none focus:border-black focus:ring-black focus:ring-1 w-full"
+					className="rounded-md border-black p-3 placeholder-grey-400 placeholder:italic placeholder:truncate focus:outline-none focus:border-black focus:ring-black focus:ring-1 w-full"
 				/>
-				</label>
 				<button
 					type="submit"
-					className="bg-black text-white py-2 px-4 rounded m-1 hover:bg-green-500"
+					className="bg-black text-white py-2 rounded mt-2 hover:bg-green-500"
 				>
 				Redo Map
 				</button>
@@ -187,21 +185,20 @@ const EditForms: React.FC<EditFormsProps> = ({ originalFormValues }) => {
 
 			{activeSection === 'table' && (
 			<form
-			className="bg-gray-200 p-4 rounded-b-md"
+			className="bg-gray-200 p-4 m-2 rounded-md flex flex-col"
 			onSubmit={(event) => handleEditForm('createTableSection', event)}
 			>
-				<label className="relative"> Table
-				<input
-					type="text"
+				<textarea
+        rows={3}
+        cols={80}
 					placeholder="Give additional information"
 					value={formStateValues.additionalInfo}
 					onChange={(event) => setFormStateValues({ ...formStateValues, additionalInfo: event.target.value })}
-					className="rounded-md border-black py-3 pl-12 pr-3 placeholder-grey-400 placeholder:italic placeholder:truncate focus:outline-none focus:border-black focus:ring-black focus:ring-1 w-full"
+					className="rounded-md border-black p-3 placeholder-grey-400 placeholder:italic placeholder:truncate focus:outline-none focus:border-black focus:ring-black focus:ring-1 w-full"
 				/>
-				</label>
 				<button
 					type="submit"
-					className="bg-black text-white py-2 px-4 rounded m-1 hover:bg-green-500"
+					className="bg-black text-white py-2 rounded mt-2 hover:bg-green-500"
 				>
 				Redo Table
 				</button>
@@ -210,21 +207,20 @@ const EditForms: React.FC<EditFormsProps> = ({ originalFormValues }) => {
 
 			{activeSection === 'footer' && (
 			<form
-			className="bg-gray-200 p-4 rounded-b-md"
+			className="bg-gray-200 p-4 m-2 rounded-md flex flex-col"
 			onSubmit={(event) => handleEditForm('createFooter', event)}
 			>
-				<label className="relative"> Footer
-				<input
-					type="text"
+				<textarea
+        rows={3}
+        cols={80}
 					placeholder="Give additional information"
 					value={formStateValues.additionalInfo}
 					onChange={(event) => setFormStateValues({ ...formStateValues, additionalInfo: event.target.value })}
-					className="rounded-md border-black py-3 pl-12 pr-3 placeholder-grey-400 placeholder:italic placeholder:truncate focus:outline-none focus:border-black focus:ring-black focus:ring-1 w-full"
+					className="rounded-md border-black p-3 placeholder-grey-400 placeholder:italic placeholder:truncate focus:outline-none focus:border-black focus:ring-black focus:ring-1 w-full"
 				/>
-				</label>
 				<button
 					type="submit"
-					className="bg-black text-white py-2 px-4 rounded m-1 hover:bg-green-500"
+					className="bg-black text-white py-2 rounded mt-2 hover:bg-green-500"
 				>
 				Redo Footer
 				</button>
