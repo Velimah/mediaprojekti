61d2162209f8ea57937230d60b4f763c9056f925
nikoprl <45226198+nikoprl@users.noreply.github.com>
2023-12-06 17:35:35 +0200

    Show tooltip when user hovers over delete button

--- src/views/Account.tsx
+++ src/views/Account.tsx
@@ -111,7 +111,7 @@ const Account: React.FC = () => {
                   <div className="w-full">
                     <button
                       onClick={() => handleDelete(data._id)}
-                      className="bg-red-500 m-2 absolute top-0 left-0 hover:bg-red-700 rounded-full border-gray-300 border p-1 font-bold arrow-path"
+                      className="group bg-red-500 m-2 absolute top-0 left-0 hover:bg-red-700 rounded-full border-gray-300 border p-1 font-bold arrow-path"
                     >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -127,6 +127,9 @@ const Account: React.FC = () => {
                           d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
                         />
                       </svg>
+                      <span className="opacity-0 group-hover:opacity-100 bg-black text-white text-xs py-1 px-2 rounded border border-gray-300 shadow-lg absolute bottom-1 left-10">
+                        DELETE
+                      </span>
                     </button>
                   </div>
                 </div>
