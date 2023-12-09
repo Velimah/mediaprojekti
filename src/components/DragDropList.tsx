aa162d5904942ef5f3f44aaa4cd99081bc91c783
nikoprl <45226198+nikoprl@users.noreply.github.com>
2023-12-09 23:22:09 +0200

    Fix "Invalid DOM property" warnings in console

--- src/components/DragDropList.tsx
+++ src/components/DragDropList.tsx
@@ -105,16 +105,16 @@ const DragDropList: React.FC<DragDropListProps> = ({ setSelectedSection, getSect
                             fill='currentColor'
                             className='w-6 h-6'
                           >
-                            <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
-                            <g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'></g>
+                            <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
+                            <g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round'></g>
                             <g id='SVGRepo_iconCarrier'>
                               {" "}
                               <path
                                 d='M5 10H19M14 19L12 21L10 19M14 5L12 3L10 5M5 14H19'
                                 stroke='#000000'
-                                stroke-width='2'
-                                stroke-linecap='round'
-                                stroke-linejoin='round'
+                                strokeWidth='2'
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
                               ></path>{" "}
                             </g>
                           </svg>
@@ -140,8 +140,8 @@ const DragDropList: React.FC<DragDropListProps> = ({ setSelectedSection, getSect
               <div className=''>
                 <span className='flex justify-center gap-2'>
                   <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' className='w-6 h-6'>
-                    <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
-                    <g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'></g>
+                    <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
+                    <g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round'></g>
                     <g id='SVGRepo_iconCarrier'>
                       {" "}
                       <g id='Interface / Trash_Full'>
@@ -150,9 +150,9 @@ const DragDropList: React.FC<DragDropListProps> = ({ setSelectedSection, getSect
                           id='Vector'
                           d='M14 10V17M10 10V17M6 6V17.8C6 18.9201 6 19.4798 6.21799 19.9076C6.40973 20.2839 6.71547 20.5905 7.0918 20.7822C7.5192 21 8.07899 21 9.19691 21H14.8031C15.921 21 16.48 21 16.9074 20.7822C17.2837 20.5905 17.5905 20.2839 17.7822 19.9076C18 19.4802 18 18.921 18 17.8031V6M6 6H8M6 6H4M8 6H16M8 6C8 5.06812 8 4.60241 8.15224 4.23486C8.35523 3.74481 8.74432 3.35523 9.23438 3.15224C9.60192 3 10.0681 3 11 3H13C13.9319 3 14.3978 3 14.7654 3.15224C15.2554 3.35523 15.6447 3.74481 15.8477 4.23486C15.9999 4.6024 16 5.06812 16 6M16 6H18M18 6H20'
                           stroke='#000000'
-                          stroke-width='2'
-                          stroke-linecap='round'
-                          stroke-linejoin='round'
+                          strokeWidth='2'
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
                         ></path>{" "}
                       </g>{" "}
                     </g>
