be8d8069f67476fcb266e28033d172ec24bf8486
nikoprl <45226198+nikoprl@users.noreply.github.com>
2023-12-11 06:34:55 +0200

    Styling changes/fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-           <div className="font-bold bg-ai-black text-ai-secondary md:m-5 p-4 h-12 w-max-screen md:rounded-md flex items-center justify-between">Footer</div>
+           <div className="font-bold text-ai-secondary bg-ai-black-100 md:m-5 p-4 h-12 w-max-screen md:rounded-md flex items-center justify-between shadow-lg">&copy; VeCaNi</div>
       );
 };
     
